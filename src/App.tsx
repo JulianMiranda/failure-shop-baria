f42dcb63266b598685673fa996954262174589ad
JulianMiranda <jmirandauria@gmail.com>
2024-08-31 09:37:03 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Failure} from './Failure';
+
+function App() {
+	return (
+		<div className="App">
+			<Failure />
+		</div>
+	);
+}
+
+export default App;
